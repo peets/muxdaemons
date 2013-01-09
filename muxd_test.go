5fa74134b82f53c1e645b96b921b9ae373932d71
peets <pierrepaulpaquin@gmail.com>
2013-01-09 10:45:47 -0500

    add log msgs. Things are fuktup

--- muxd_test.go
+++ muxd_test.go
@@ -3,6 +3,7 @@ package muxdaemons
 import (
 	"testing"
 	"os/exec"
+	"strings"
 )
 
 // check that we can load & run the test scripts
@@ -16,71 +17,84 @@ func TestCwd(t *testing.T) {
 	}
 }
 
-/*
-nope error
-0x retire | 1x print
-1x retire
-x1 print 1
-x1 print 2 | x2 print 1
-x1 print 3 | x3 print 1
-x1 print 4 | x2 print 2
-x1 print 5
-x1 retire
-x2 print 3 | x3 print 2
-x2 retire | x3 retire
-*/
-
 type eventType int
 const (
-	out eventType= iota
+	out eventType = iota
 	ded
 )
-type event struct {
-	eventType eventType
-	command   string
-	commandNo int
+
+func checkLine(line [6]int, ref string) bool {
+	strLine := make([]string, 6)
+	for i, n := range line {
+		if n == 0 {
+			strLine[i] = ""
+		} else {
+			strLine[i] = string(n)
+		}
+	}
+	return strings.Join(strLine, " ") == ref
 }
 
 func TestRun(t *testing.T) {
-	events := [][]event{
-		{event{ded, "testdata/nope", 5}},
-		{event{out, "testdata/1x0"}, event{ded, "testdata/0x"}},
-		{event{ded, "testdata/1x0"}},
-		{event{out, "testdata/5x1"}},
-		{event{out, "testdata/5x1"}, event{out, "testdata/3x2"}},
-		{event{out, "testdata/5x1"}, event{out, "testdata/2x3"}},
-		{event{out, "testdata/5x1"}, event{out, "testdata/3x2"}},
-		{event{out, "testdata/5x1"}},
-		{event{ded, "testdata/5x1"}},
-		{event{out, "testdata/3x2"}, event{out, "testdata/2x3"}},
-		{event{ded, "testdata/3x2"}, event{ded, "testdata/2x3"}}
+	events := []map[eventType] map[int] bool {
+		{ded: {0: true, 4: true, 5: true}, out: {0: true}},
+		{out: {1: true}},
+		{out: {1: true, 2: true}},
+		{out: {1: true, 3: true}},
+		{out: {1: true, 2: true}},
+		{out: {1: true}, ded: {1: true}},
+		{out: {2: true, 3: true}, ded: {2: true, 3: true}},
 	}
+	/*
+	events := []map[eventType] map[int] bool {
+		{ded: {4: true}},
+		{ded: {3: true}},
+		{out: {0: true}},
+		{out: {0: true, 1: true}},
+		{out: {0: true, 2: true}},
+		{out: {0: true, 1: true}},
+		{out: {0: true}, ded: {0: true}},
+		{out: {1: true, 2: true}, ded: {1: true, 2: true}},
+	}
+	*/
 	outc := make(chan string)
-	retiredc := make(chan *muxdaemons.Retired)
+	retiredc := make(chan *Retired)
 	commands := []string{"testdata/1x0", "testdata/5x1", "testdata/3x2", "testdata/2x3", "testdata/0x", "testdata/nope"}
-	go muxdaemons.Run(commands, outc, retiredc)
+//	commands := []string{"testdata/5x1", "testdata/3x2", "testdata/2x3", "testdata/0x", "testdata/nope"}
+	go Run(commands, outc, retiredc)
 	var line [6]int
-	for i, simultaneousEvents := range events {
-		select {
-		case retiree := <-retiredc:
-			ok := false
-			for j, event := range events {
-				if event.eventType == ded && event.payload == commands[retiree] {
-					ok = true
-
-					break
+	for _, simultaneousEvents := range events {
+		t.Logf("checking %v\n", simultaneousEvents)
+		for len(simultaneousEvents) > 0 {
+			select {
+			case retiree := <-retiredc:
+				t.Logf("got retiree %v\n", retiree)
+				if simultaneousEvents[ded][retiree.Index] {
+					delete(simultaneousEvents[ded], retiree.Index)
+					if len(simultaneousEvents[ded]) == 0 {
+						delete(simultaneousEvents, ded)
+					}
+				} else {
+					t.Fatalf("wrongful death: got %d, expected one of %v", retiree.Index, simultaneousEvents[ded])
 				}
-			}
-			if !ok {
-				t.Fatalf("wrongful death: got %s, expected one of %v", commands[retiree], events)
-			}
-		case out := <-outc:
-			// I ain't gone be a slave to the data structure.
-			// What I want to do is this: if I have simultaneous events, try out each possibility. Whichever one works out (if any),
-			ok := false
-			for j, event := range events {
-				if event.eventType == out {
-					line[event.commandNo] += 1
+			case output := <-outc:
+				t.Logf("got output %s\n", output)
+				ok := false
+				for c, _ := range simultaneousEvents[out] {
+					tentativeLine := line
+					tentativeLine[c] += 1
+					if(checkLine(tentativeLine, output)) {
+						line[c] += 1
+						delete(simultaneousEvents[out], c)
+						if len(simultaneousEvents[out]) == 0 {
+							delete(simultaneousEvents, out)
+						}
+						ok = true
+						break
+					}
+				}
+				if !ok {
+					t.Fatalf("output '%s' cannot be reached from line '%v' with any of these events: %v", output, line, simultaneousEvents[out])
 				}
 			}
 		}
--- muxdaemons.go
+++ muxdaemons.go
@@ -5,6 +5,7 @@ import (
 	"os/exec"
 	"strings"
 	"bufio"
+	"log"
 )
 
 // Represents a retired daemon (a deamon that errored out)
@@ -51,11 +52,12 @@ func Run(commands []string, out chan string, retirements chan *Retired) {
 	errc := make(chan int)
 	sync := make(chan int)
 	for i, d := range daemons {
-		go func() {
+		go func(i int, d *daemon) {
 			lines := make(chan string, 1)
-			myErrc := make(chan error)
+			myErrc := make(chan error, 1)
 			line := ""
 
+			log.Printf("%d (%s) execing\n", i, d.command)
 			cmd := exec.Command(d.command)
 			cmdOut, err := cmd.StdoutPipe()
 			if err != nil {
@@ -64,25 +66,30 @@ func Run(commands []string, out chan string, retirements chan *Retired) {
 				return
 			}
 			bufCmdOut := bufio.NewReader(cmdOut)
+			log.Printf("%d (%s) starting\n", i, d.command)
 			err = cmd.Start()
 			if err != nil {
 				d.err = err
 				errc <- i
 				return
 			}
+			log.Printf("%d (%s) started\n", i, d.command)
 			for {
 				select {
 				case lines <- teeLine(bufCmdOut, myErrc):
+					log.Printf("%d (%s) teeLine out\n", i, d.command)
 				case line = <-lines:
+					log.Printf("%d (%s) line consumed\n", i, d.command)
 					d.out <- line
 					sync <- i
 				case err := <-myErrc:
+					log.Printf("%d (%s) error consumed\n", i, d.command)
 					d.err = err
 					errc <- i
 					return
 				}
 			}
-		}()
+		}(i, d)
 	}
 
 	// mux output as it arrives
@@ -90,9 +97,12 @@ func Run(commands []string, out chan string, retirements chan *Retired) {
 	for {
 		select {
 		case i := <-errc:
+			log.Printf("sending retirement %d\n", i)
 			retirements <- &Retired{i, daemons[i].command, daemons[i].err}
 		case i := <-sync:
+			log.Printf("got sync %d\n", i)
 			lines[i] = strings.TrimRight(<-daemons[i].out, "\n")
+			log.Printf("sending line '%v'\n", lines)
 			out <- strings.Join(lines, " ")
 		}
 	}
